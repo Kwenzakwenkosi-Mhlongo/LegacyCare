e1fa4243daef6f2a3d1c49412ef47b3df5815ec1
Kwenzakwenkosi Mhlongo <mkaykwenza@gmail.com>
2026-08-12 11:37:23 +0200

    Configure HTTPS email service

--- legacycare_backend/legacycare_backend/Program.cs
+++ legacycare_backend/legacycare_backend/Program.cs
@@ -216,4 +216,6 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
+app.Run($"http://0.0.0.0:{port}");
